f5e2f8159e7ec58b621ce5033128b3edc95b2a30
unknown <igor.lakic2@gmail.com>
2022-09-29 22:24:15 +0200

    routing done, admin and public routes added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import "./index.css";
+import Admin from "./pages/Admin";
+import Login from "./pages/Login";
+import Public from "./pages/Public";
+import { HomeLayout } from "./routing/HomeLayout";
+import { ProtectedLayout } from "./routing/ProtectedLayout";
 
 function App() {
   return (
-    <div className="App">
-      <h1>news</h1>
-    </div>
+    <Routes>
+      <Route element={<HomeLayout />}>
+        <Route path="/" element={<Public />} />
+        <Route path="/prijava" element={<Login />} />
+      </Route>
+
+      <Route path="/admin-panel" element={<ProtectedLayout />}>
+        <Route path="" element={<Admin />} />
+      </Route>
+    </Routes>
   );
 }
 
---
+++ src/routing/ProtectedLayout.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Navigate, useOutlet } from "react-router-dom";
+import { useAuth } from "./useAuth";
+// import AdminHeader from "../components/Header/AdminHeader";
+// ...
+
+export const ProtectedLayout = () => {
+  const { user } = useAuth();
+  const outlet = useOutlet();
+
+  if (!user) {
+    return <Navigate to="/" />;
+  }
+
+  return (
+    <div>
+      {/* <AdminHeader /> */}
+      {outlet}
+    </div>
+  );
+};
